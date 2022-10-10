8beb43788fbfdfc90946ced81c131fffe867d479
Karsyz <mattkars@gmail.com>
2022-10-09 21:50:04 -0400

    populating user data in dashboard from db

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,6 +1,18 @@
+const User = require("../models/User")
+
 module.exports = {
-    getDashboard: (req, res) => {
-      res.render("dashboard.ejs", {user: req.user });
+    getDashboard: async (req, res) => {
+      // incomplete
+      // add all user objects from in members array
+      console.log(req.user.members)
+      const allUsers = []
+      for(let i = 0; i < req.user.members.length; i++) {
+        allUsers.push( await User.findById(req.user.members[i]).lean() )
+      }
+    res.render("dashboard.ejs", {user: req.user, allUsers: allUsers });
+    console.log(allUsers)
     },
-  };
+  }
   
+  
+  // await User.findById(el).lean()
