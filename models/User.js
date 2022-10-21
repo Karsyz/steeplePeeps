dfeffd13aa44de7cc26c0d7d7212d5a6dddd556b
Karsyz <mattkars@gmail.com>
2022-10-20 22:31:38 -0400

    started work on magic link

--- models/User.js
+++ models/User.js
@@ -89,6 +89,16 @@ const UserSchema = new mongoose.Schema({
     type: Number,
     require: true,
   },
+  magicLink: {
+    type: String,
+    required: false,
+    unique: false,
+    default: uuidv4,
+  },
+  magicLinkExpired: {
+    type: Boolean,
+    default: false,
+  }
 });
 
 // Password hash middleware.
