1f45737b1b393b8dc6361984d130c92a09e62ac2
Karsyz <mattkars@gmail.com>
2022-11-08 20:13:02 -0500

    cleaned up darkMode.js obsolete code

--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -30,64 +30,9 @@ dmSwich.addEventListener('click', () => {
 function dark() {
   document.querySelector('body').classList.add('theme-dark')
   document.querySelector('body').classList.remove('theme-light')
-  // document.querySelector('body').classList.add('bg-dark')
-  // document.querySelector('body').classList.add('text-light')
-  
-  // const ts = document.querySelectorAll('.ts')
-  // ts.forEach( e => e.classList.add('text-light') )
-
-  // const formLabels = document.querySelectorAll('.form-label')
-  // formLabels.forEach( e => e.classList.add('text-light') )
-
-  // // tables
-  // if (document.querySelector('table') !== null) {
-  //   document.querySelector('table').classList.add('table-dark')
-  //   document.querySelector('thead').classList.add('table-secondary')
-  //   document.querySelector('thead').classList.remove('table-dark')
-  // }
-
-  // const tda = document.querySelectorAll('.text-dark')
-  // tda.forEach( e => e.classList.add('text-light') )
-
-  // const tdb = document.querySelectorAll('.text-dark')
-  // tdb.forEach( e => e.classList.remove('text-dark') )
-
-  // document.querySelector('.imageUploadContainerInner').classList.add('bg-dark')
-
-  // document.querySelector('.imgPlaceholder').classList.add('bg-dark')
-
-  
-  // const imgThumb = document.querySelectorAll('.img-thumbnail')
-  // imgThumb.forEach( e => e.classList.remove('bg-dark') )
-
 }
 
 function light() {
   document.querySelector('body').classList.add('theme-light')
   document.querySelector('body').classList.remove('theme-dark')
-//   document.querySelector('body').classList.remove('bg-dark')
-//   document.querySelector('body').classList.remove('text-light')
-  
-//   const ts = document.querySelectorAll('.ts')
-//   ts.forEach( e => e.classList.remove('text-light') )
-
-//   const formLabels = document.querySelectorAll('.form-label')
-//   formLabels.forEach( e => e.classList.remove('text-light') )
-
-//   // tables
-//   if (document.querySelector('table') !== null) {
-//     document.querySelector('table').classList.remove('table-dark')
-//     document.querySelector('thead').classList.remove('table-secondary')
-//     document.querySelector('thead').classList.add('table-dark')
-//   }
-
-//   const tdb = document.querySelectorAll('.text-light')
-//   tdb.forEach( e => e.classList.add('text-dark') )
-
-//   const tda = document.querySelectorAll('.text-light')
-//   tda.forEach( e => e.classList.remove('text-light') )
-
-//   document.querySelector('.imageUploadContainerInner').classList.remove('bg-dark')
-//   document.querySelector('.imgPlaceholder').classList.remove('bg-dark')
-
 }
