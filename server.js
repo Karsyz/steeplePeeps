cd8ba31e703473c5d6d71c0160b0c3fd2c8e195d
Karsyz <mattkars@gmail.com>
2023-09-15 14:41:54 -0400

    updated listen port logic

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose.connect(process.env.DB_STRING, {
 })
 .then(() => {
   // listen for requests
-  app.listen(process.env.PORT, () => {
+  app.listen(process.env.PORT || 3000,  () => {
     console.log(`Server is listening on port ${process.env.PORT}`)
   })
 })
