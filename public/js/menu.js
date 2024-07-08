f965e09cc49918ffe6a0a7238f87f45fc4f4829e
Matt Kars <mattkars@gmail.com>
2024-07-08 14:47:59 -0400

    refactor: directory page layout and nav styling

--- public/js/menu.js
+++ public/js/menu.js
@@ -7,8 +7,9 @@ const openAddUserButton = document.querySelector('#openAddUserButton')
 const closeAddUserButton = document.querySelector('#closeAddUserButton')
 
 menu.addEventListener('click', () => {
-  navCont.classList.remove('translate-x-full')
-  navCont.classList.add('translate-x-0')
+  // navCont.classList.remove('translate-x-full')
+  // navCont.classList.add('translate-x-0')
+  navCont.classList.toggle('hidden')
 })
 
 menuClose.addEventListener('click', deactivateMenu)
